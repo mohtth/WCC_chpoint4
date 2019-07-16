869ab637971b46c6f6ef002cd39436a90f91f5cd
Tth <mtoutouhi@gmail.com>
2019-07-16 11:50:44 +0200

    debut projet

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../scss/app.scss');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+}
